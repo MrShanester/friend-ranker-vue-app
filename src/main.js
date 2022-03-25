8258f79f9e7803b341138c0e5294841e62c7b17e
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-24 20:03:40 -0700

    Adds axios npm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "https://dynamic-solar-system-app.herokuapp.com" : "/";
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
 
 Vue.config.productionTip = false;
 
